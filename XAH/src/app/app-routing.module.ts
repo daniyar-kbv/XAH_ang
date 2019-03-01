57fd612084979aa422feda74c9dfa14523cb464b
Almaz Mukhanov <mukhanov2511@gmail.com>
2019-03-01 19:18:16 +0600

    ng added

--- XAH/src/app/app-routing.module.ts
+++ XAH/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { PostComponent } from './post/post.component';
 
 const routes: Routes = [
-    {path: 'main', component: MainComponent}
+    { path: 'main', component: MainComponent },
+    { path: 'post', component: PostComponent }
 ];
 
 @NgModule({
