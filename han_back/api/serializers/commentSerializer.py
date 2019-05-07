f9a991e895bb3390b9bf93875ec7c04c4cb35938
daniyarka <daniyar_k-98@mail.ru>
2019-05-07 20:42:00 +0600

    comment create

--- han_back/api/serializers/commentSerializer.py
+++ han_back/api/serializers/commentSerializer.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .userSerializer import UserModelSerializer
 from .articleSerializer import ArticleSerializer
+from ..models import Comment
 
 
 class CommentSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     body = serializers.CharField(max_length=255)
-    date_published = serializers.DateTimeField()
+    date_published = serializers.DateTimeField(read_only=True)
     created_by = UserModelSerializer(read_only=True)
-    article = ArticleSerializer
+    article = ArticleSerializer(read_only=True)
+
+    def create(self, validated_data):
+        return Comment.objects.create(**validated_data)
--- han_back/api/urls.py
+++ han_back/api/urls.py
@@ -3,10 +3,15 @@ from django.urls import path
 from .views.articleLikeViews import articleLike_list
 from .views.auth import Register
 from rest_framework_jwt.views import obtain_jwt_token
+from .views.commentView import CommentList, CommentCreate
 
 
 urlpatterns = [
     path('articles/<int:pk>/likes/', articleLike_list.as_view()),
     path('register/', Register.as_view()),
     path('login/', obtain_jwt_token),
+
+    # Comments
+    path('articles/<int:article_id>/comments/', CommentList.as_view()),
+    path('articles/<int:article_id>/comments/create/', CommentCreate.as_view())
 ]
