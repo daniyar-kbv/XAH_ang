f99f0fb9e966a3d561bc987431d3b2d07235a0a0
lgracel <mukhanov2511@gmail.com>
2019-05-07 17:31:47 +0600

    commentLikes urls added

--- han_back/api/urls.py
+++ han_back/api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views.articleLikeViews import articleLike_list, articleLike_delete
+from .views.commentLikeView import xommentLike_list, commentLike_delete
 from .views.auth import Register
 from rest_framework_jwt.views import obtain_jwt_token
 
@@ -8,6 +9,10 @@ from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path('articles/<int:pk>/likes/', articleLike_list()),
     path('articles/<int:pk>/likes/<int:pk>/', articleLike_delete.as_view()),
+
+    path('comments/<int:pk>/likes/', commentLike_list()),
+    path('comments/<int:pk>/likes/<int:pk>/', articleLike_delete.as_view()),
+
     path('register/', Register.as_view()),
     path('login/', obtain_jwt_token),
 ]
