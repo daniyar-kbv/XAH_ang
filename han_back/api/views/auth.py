92ea41b497c7efc6c2d12bccfc144457c12c0a57
Kulyash Konyrova <konyrova@gmail.com>
2019-05-13 15:43:07 +0600

    registartion to token

--- han_back/api/views/auth.py
+++ han_back/api/views/auth.py
@@ -1,10 +1,38 @@
-from rest_framework.generics import CreateAPIView
-from ..serializers.userSerializer import UserModelSerializer
+from django.contrib.auth.models import User
+from api.serializers import UserModelSerializer
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.models import Token
 
-class Register(CreateAPIView):
-    authentication_classes = ()
-    permission_classes = ()
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
     serializer_class = UserModelSerializer
 
-    def perform_create(self, serializer):
-        return serializer.save()
+
+@api_view(['POST'])
+def login(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data['user']
+    token, created = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key})
+
+
+@api_view(['POST'])
+def logout(request):
+    request.auth.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserModelSerializer(data=request.data)
+    if serializer.is_valid():
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
