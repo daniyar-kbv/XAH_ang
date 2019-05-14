20f678f686bc8a0c59b3d442a0edaf08cccf23a7
Kulyash Konyrova <konyrova@gmail.com>
2019-05-14 21:31:41 +0600

    sync

--- XAH/src/app/shared/services/provider.service.ts
+++ XAH/src/app/shared/services/provider.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MainService } from './main.service';
 import { HttpClient } from '@angular/common/http';
-import { IAuthResponse } from '../models/authResponse'
+import { IAuthResponse } from '../models/authResponse';
 import {IArticle} from '../models/article';
 import {IComment} from '../models/comment';
 import {IUser} from '../models/user';
@@ -10,7 +10,7 @@ import {IUser} from '../models/user';
 @Injectable({
   providedIn: 'root'
 })
-export class ProviderService extends MainService{
+export class ProviderService extends MainService {
 
   private baseUrl = 'http://127.0.0.1:8000/api/';
 
@@ -18,34 +18,38 @@ export class ProviderService extends MainService{
     super(http);
   }
 
-  getComments(articleId: any): Promise<IComment[]>{
-    return this.get(this.baseUrl + `articles/${articleId}/comments/`, {})
+  getComments(articleId: any): Promise<IComment[]> {
+    return this.get(this.baseUrl + `articles/${articleId}/comments/`, {});
   }
 
-  login(username: any, password: any): Promise<IAuthResponse>{
-    return this.post( this.baseUrl + 'login/', {
-      username: username,
-      password: password
-    })
+  login(username: any, password: any): Promise<IAuthResponse> {
+    return this.post(this.baseUrl + 'login/', {
+      username,
+      password
+    });
   }
 
-  register(username: any, password: any, email: any): Promise<IUser>{
-    return this.post(  this.baseUrl + 'register/', {
-      username: username,
-      password: password,
-      email: email
-    })
+  register(username: any, password: any, email: any): Promise<IUser> {
+    return this.post(this.baseUrl + 'register/', {
+      username,
+      password,
+      email
+    });
   }
-  
+
   logout(): Promise<any> {
     return this.post(this.baseUrl + 'logout/', {});
   }
 
-  getArticle(): Promise<IArticle>{
-    return this.get(`http://localhost:8000/api/articles/1/`, {})
+  getArticle(): Promise<IArticle> {
+    return this.get(this.baseUrl + 'articles/1/', {});
   }
 
-  putLike(){
-    return this.post(`http://localhost:8000/api/articles/1/likes/`, {})
+  putArticleLike() {
+    return this.post(this.baseUrl + 'articles/1/likes/', {});
+  }
+
+  getArticleLikes() {
+    return this.get(this.baseUrl + 'articles/1/likes/', {});
   }
 }
